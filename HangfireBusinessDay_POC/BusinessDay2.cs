50e02ae7c0f61144f84065caffbbcc510db0e067
chisty sohel <chistysohail@gmail.com>
2023-12-14 22:39:10 -0500

    corrected and updated logic

--- HangfireBusinessDay_POC/BusinessDay2.cs
+++ HangfireBusinessDay_POC/BusinessDay2.cs
@@ -11,9 +11,7 @@ namespace HangfireBusinessDay_POC
     // instance method filter 
     public class BusinessDay2FilterAttribute : JobFilterAttribute, IElectStateFilter
     {
-        
-
-        // In the OnStateElection method, you would use this method like this:
+      
         public void OnStateElection(ElectStateContext context)
         {
             var enqueuedState = context.CandidateState as EnqueuedState;
@@ -23,39 +21,40 @@ namespace HangfireBusinessDay_POC
                 context.CandidateState = new ScheduledState(nextBusinessDayAt8AmUtc);
             }
         }
-
-
-       
+                
+        
         private bool IsBusinessDay(DateTime date)
         {
-            // Business day is any weekday (Monday to Friday)
+            // Assuming business days are Monday to Friday.
             return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
         }
 
-       
+
         private DateTime GetNextBusinessDay(DateTime currentDate)
         {
-            // If today is a business day and it's before 8 AM UTC, schedule for today at 8 AM UTC.
+            // Check if today is a business day and the time is before 8 AM UTC.
             if (IsBusinessDay(currentDate) && currentDate.TimeOfDay < TimeSpan.FromHours(8))
             {
+                // If it's before 8 AM, set the time to 8 AM UTC today.
                 return new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 8, 0, 0, DateTimeKind.Utc);
             }
 
-            // Otherwise, find the next business day.
-            DateTime nextBusinessDay = currentDate.Date.AddDays(1);
-            while (!IsBusinessDay(nextBusinessDay))
+            // If it's not a business day or past 8 AM, find the next business day.
+            DateTime nextBusinessDay = currentDate.AddDays(1);
+            // Loop to skip weekends.
+            while (nextBusinessDay.DayOfWeek == DayOfWeek.Saturday || nextBusinessDay.DayOfWeek == DayOfWeek.Sunday)
             {
                 nextBusinessDay = nextBusinessDay.AddDays(1);
             }
 
-            // Return 8 AM UTC on the next business day.
+            // Set the time to 8 AM UTC on the next business day.
             return new DateTime(nextBusinessDay.Year, nextBusinessDay.Month, nextBusinessDay.Day, 8, 0, 0, DateTimeKind.Utc);
         }
 
-       
+        
+      
 
-       
-       
+
     }
 
     public class MyJobClass
