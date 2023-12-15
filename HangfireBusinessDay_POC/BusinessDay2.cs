d2f34ee6cb1710db811a66bc9d2e0e74d5ee8a61
chisty sohel <chistysohail@gmail.com>
2023-12-14 23:02:55 -0500

    updated to schedule on current run time but just check the Day

--- HangfireBusinessDay_POC/BusinessDay2.cs
+++ HangfireBusinessDay_POC/BusinessDay2.cs
@@ -11,48 +11,47 @@ namespace HangfireBusinessDay_POC
     // instance method filter 
     public class BusinessDay2FilterAttribute : JobFilterAttribute, IElectStateFilter
     {
-      
+
+        private DateTime GetNextBusinessDay(DateTime currentDate)
+        {
+            // Check if today is a business day (Monday through Friday).
+            if (IsBusinessDay(currentDate))
+            {
+                // If it is a business day, return the current date and time.
+                return currentDate;
+            }
+
+            // If it's not a business day (Saturday or Sunday), find the next Monday.
+            DateTime nextBusinessDay = currentDate;
+            while (nextBusinessDay.DayOfWeek != DayOfWeek.Monday)
+            {
+                nextBusinessDay = nextBusinessDay.AddDays(1);
+            }
+
+            // Set the time to the same time as the current day.
+            return new DateTime(nextBusinessDay.Year, nextBusinessDay.Month, nextBusinessDay.Day,
+                                currentDate.Hour, currentDate.Minute, currentDate.Second, DateTimeKind.Utc);
+        }
+
+        private bool IsBusinessDay(DateTime date)
+        {
+            // Business day is any weekday (Monday to Friday).
+            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+        }
+
+        // The OnStateElection method should look like this:
         public void OnStateElection(ElectStateContext context)
         {
             var enqueuedState = context.CandidateState as EnqueuedState;
             if (enqueuedState != null)
             {
-                var nextBusinessDayAt8AmUtc = GetNextBusinessDay(DateTime.UtcNow);
-                context.CandidateState = new ScheduledState(nextBusinessDayAt8AmUtc);
+                var nextTimeToRun = GetNextBusinessDay(DateTime.UtcNow);
+                context.CandidateState = new ScheduledState(nextTimeToRun);
             }
         }
-                
-        
-        private bool IsBusinessDay(DateTime date)
-        {
-            // Assuming business days are Monday to Friday.
-            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
-        }
 
 
-        private DateTime GetNextBusinessDay(DateTime currentDate)
-        {
-            // Check if today is a business day and the time is before 8 AM UTC.
-            if (IsBusinessDay(currentDate) && currentDate.TimeOfDay < TimeSpan.FromHours(8))
-            {
-                // If it's before 8 AM, set the time to 8 AM UTC today.
-                return new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, 8, 0, 0, DateTimeKind.Utc);
-            }
 
-            // If it's not a business day or past 8 AM, find the next business day.
-            DateTime nextBusinessDay = currentDate.AddDays(1);
-            // Loop to skip weekends.
-            while (nextBusinessDay.DayOfWeek == DayOfWeek.Saturday || nextBusinessDay.DayOfWeek == DayOfWeek.Sunday)
-            {
-                nextBusinessDay = nextBusinessDay.AddDays(1);
-            }
-
-            // Set the time to 8 AM UTC on the next business day.
-            return new DateTime(nextBusinessDay.Year, nextBusinessDay.Month, nextBusinessDay.Day, 8, 0, 0, DateTimeKind.Utc);
-        }
-
-        
-      
 
 
     }
