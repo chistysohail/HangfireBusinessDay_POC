a1dcba3f2032445bacedf1c1f81c2844e826cfac
chisty sohel <chistysohail@gmail.com>
2023-12-15 21:34:49 -0500

    Updated Filter logic

--- HangfireBusinessDay_POC/BusinessDay2.cs
+++ HangfireBusinessDay_POC/BusinessDay2.cs
@@ -1,10 +1,5 @@
 ﻿using Hangfire.Common;
 using Hangfire.States;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HangfireBusinessDay_POC
 {
@@ -14,41 +9,27 @@ namespace HangfireBusinessDay_POC
         public void OnStateElection(ElectStateContext context)
         {
             var enqueuedState = context.CandidateState as EnqueuedState;
-            if (enqueuedState != null)
+            if (enqueuedState != null && !IsBusinessDay(DateTime.Now))
             {
-                var nextTimeToRun = GetNextBusinessDay(DateTime.UtcNow);
-                context.CandidateState = new ScheduledState(nextTimeToRun);
+                var nextBusinessDay = GetNextBusinessDay(DateTime.Now);
+                context.CandidateState = new ScheduledState(nextBusinessDay);
             }
         }
+
         private bool IsBusinessDay(DateTime date)
         {
-            // Business day is any weekday (Monday to Friday).
             return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
         }
+
         private DateTime GetNextBusinessDay(DateTime currentDate)
         {
-            // Check if today is a business day (Monday through Friday).
-            if (IsBusinessDay(currentDate))
-            {
-                // If it is a business day, return the current date and time.
-                return currentDate;
-            }
-
-            // If it's not a business day (Saturday or Sunday), find the next Monday.
             DateTime nextBusinessDay = currentDate;
-            while (nextBusinessDay.DayOfWeek != DayOfWeek.Monday)
+            while (!IsBusinessDay(nextBusinessDay))
             {
                 nextBusinessDay = nextBusinessDay.AddDays(1);
             }
-
-            // Set the time to the same time as the current day.
-            return new DateTime(nextBusinessDay.Year, nextBusinessDay.Month, nextBusinessDay.Day,
-                                currentDate.Hour, currentDate.Minute, currentDate.Second, DateTimeKind.Utc);
+            return nextBusinessDay.Date;
         }
-        
-        
-
-
 
     }
 
